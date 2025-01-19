27d56bc55eb9f0b2c448d75a43925b6023483f94
msl <maxsloeffler@gmail.com>
2025-01-19 09:40:19 -0500

    prepare for subtree

--- experiments/repeng/__init__.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-
-import numpy as np
-import torch
-from transformers import PreTrainedModel, PreTrainedTokenizerBase
-
-from . import control, extract
-from .extract import ControlVector, DatasetEntry
-from .control import ControlModel
